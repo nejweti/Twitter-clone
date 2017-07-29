daeccdb3bf5ea10b1ff0d576962aa839ae4386df
nejat <nejatsultan5@gmail.com>
2017-07-29 09:39:37 +0000

    route Configured

---
+++ flutter/app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def index
+  end
+
+  def profile
+  end
+
+  def explore
+  end
+end
--- flutter/config/routes.rb
+++ flutter/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  
+  root 'pages#index'
+  get '/home' => 'pages#home'
+  get 'pages/profile' => 'pages#profile'
+  get 'pages/explore' => 'pages#explore'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
